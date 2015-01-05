812de94481f6079337fd0d003a2f6b546065767d
Katie Hoffman <ktahoffman@gmail.com>
2015-01-05 11:57:56 -0500

    add option to pluralize 'cookie'

--- spec/key_terms_spec.rb
+++ spec/key_terms_spec.rb
@@ -1,82 +1,82 @@
 describe "Key Terms Challenge" do
   it 'is a text-only way of browsing and interacting with your computer' do
     your_answer = __ # replace the __ with your answers in "quotes"
-    expect(your_answer).to eq(answer_1)
+    expect(your_answer).to satisfy {|s| answer_1.include?(s.downcase) }
   end
 
   it 'keeps track of the changes you make to a file' do
     your_answer = __
-    expect(your_answer).to eq(answer_2)
+    expect(your_answer).to satisfy {|s| answer_2.include?(s.downcase) }
   end
 
   it 'downloads onto your computer to keep track of what you do on its site' do
     your_answer = __
-    expect(your_answer).to eq(answer_3)
+    expect(your_answer).to satisfy {|s| answer_3.include?(s.downcase) }
   end
 
   it 'is the status code for “website not found”' do
     your_answer = __
-    expect(your_answer).to eq(answer_4)
+    expect(your_answer).to satisfy {|s| answer_4.include?(s.downcase) }
   end
 
   it 'is a computer that can be accessed through the Internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_5)
+    expect(your_answer).to satisfy {|s| answer_5.include?(s.downcase) }
   end
 
   it 'is your computers address on the internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_6)
+    expect(your_answer).to satisfy {|s| answer_6.include?(s.downcase) }
   end
 
   it 'keeps track of domain names and IP address on the internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_7)
+    expect(your_answer).to satisfy {|s| answer_7.include?(s.downcase) }
   end
 
   it 'is a way of designing a web page to be easily viewable on multiple screen sizes' do 
     your_answer = __
-    expect(your_answer).to eq(answer_8)
+    expect(your_answer).to satisfy {|s| answer_8.include?(s.downcase) }
   end
 
   it 'is the way web browsers describe colors' do 
     your_answer = __
-    expect(your_answer).to eq(answer_9)
+    expect(your_answer).to satisfy {|s| answer_9.include?(s.downcase) }
   end
 
   it 'is the (secure) way data is moved around the internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_10)
+    expect(your_answer).to satisfy {|s| answer_10.include?(s.downcase) }
   end
 
   it 'is the practice of sharing how a computer program was made and allowing anyone to customize the program as they see fit' do 
     your_answer = __
-    expect(your_answer).to eq(answer_11)
+    expect(your_answer).to satisfy {|s| answer_11.include?(s.downcase) }
   end
 
   it 'makes browser requests to the server asynchronous' do 
     your_answer = __
-    expect(your_answer).to eq(answer_12)
+    expect(your_answer).to satisfy {|s| answer_12.include?(s.downcase) }
   end
 
   it 'is the programming language of the browser' do 
     your_answer = __
-    expect(your_answer).to eq(answer_13)
+    expect(your_answer).to satisfy {|s| answer_13.include?(s.downcase) }
   end
 
   it 'holds all the data in your application' do 
     your_answer = __
-    expect(your_answer).to eq(answer_14)
+    expect(your_answer).to satisfy {|s| answer_14.include?(s.downcase) }
   end
 
   it 'is the language to build a websites content' do 
     your_answer = __
-    expect(your_answer).to eq(answer_15)
+    expect(your_answer).to satisfy {|s| answer_15.include?(s.downcase) }
   end
 
   it 'is the language to style a websites content' do 
     your_answer = __
-    expect(your_answer).to eq(answer_16)
+    expect(your_answer).to satisfy {|s| answer_16.include?(s.downcase) }
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,18 @@
 require 'base64'
+require 'json'
 
 RSpec.configure do |config|
-  encoded_answers = "Y29tbWFuZCBsaW5lLCB2ZXJzaW9uIGNvbnRyb2wsIGNvb2tpZXMsIDQwNCBl\ncnJvciwgd2ViIHNlcnZlciwgSVAgYWRkcmVzcywgRE5TLCByZXNwb25zaXZl\nIGRlc2lnbiwgaGV4LCBIVFRQUywgb3BlbiBzb3VyY2UsIEFKQVgsIEphdmFz\nY3JpcHQsIGRhdGFiYXNlLCBIVE1MLCBDU1M=\n" 
-  decoded_answers = Base64.decode64(encoded_answers)
+  encoded_answers = "eyIxIjpbImNvbW1hbmQgbGluZSJdLCIyIjpbInZlcnNpb24gY29udHJvbCJd\nLCIzIjpbImNvb2tpZXMiLCJjb29raWUiXSwiNCI6WyI0MDQgZXJyb3IiXSwi\nNSI6WyJ3ZWIgc2VydmVyIl0sIjYiOlsiaXAgYWRkcmVzcyJdLCI3IjpbImRu\ncyJdLCI4IjpbInJlc3BvbnNpdmUgZGVzaWduIl0sIjkiOlsiaGV4Il0sIjEw\nIjpbImh0dHBzIl0sIjExIjpbIm9wZW4gc291cmNlIl0sIjEyIjpbImFqYXgi\nXSwiMTMiOlsiamF2YXNjcmlwdCJdLCIxNCI6WyJkYXRhYmFzZSJdLCIxNSI6\nWyJodG1sIl0sIjE2IjpbImNzcyJdfQ==\n"
+  decoded_json = Base64.decode64(encoded_answers)
+  answers = JSON.parse(decoded_json)
 
-  answers = decoded_answers.split(", ")
-
-  answers.each_with_index do |answer, index|
-    define_method("answer_#{index+1}") do 
-      answer
+  answers.each do |number, answer_array|
+    define_method("answer_#{number}") do 
+      answer_array
     end
   end
+end
+
+def __
+  raise 'Replace the underscore with your answer in "quotes"'
 end
