c631466fac904eab958075d40ef0ba09fbab4713
Katie Hoffman <ktahoffman@gmail.com>
2015-01-06 10:44:08 -0500

    make only answer 3 have options of answers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ require 'base64'
 require 'json'
 
 RSpec.configure do |config|
-  encoded_answers = "eyIxIjpbImNvbW1hbmQgbGluZSJdLCIyIjpbInZlcnNpb24gY29udHJvbCJd\nLCIzIjpbImNvb2tpZXMiLCJjb29raWUiXSwiNCI6WyI0MDQgZXJyb3IiXSwi\nNSI6WyJ3ZWIgc2VydmVyIl0sIjYiOlsiaXAgYWRkcmVzcyJdLCI3IjpbImRu\ncyJdLCI4IjpbInJlc3BvbnNpdmUgZGVzaWduIl0sIjkiOlsiaGV4Il0sIjEw\nIjpbImh0dHBzIl0sIjExIjpbIm9wZW4gc291cmNlIl0sIjEyIjpbImFqYXgi\nXSwiMTMiOlsiamF2YXNjcmlwdCJdLCIxNCI6WyJkYXRhYmFzZSJdLCIxNSI6\nWyJodG1sIl0sIjE2IjpbImNzcyJdfQ==\n"
+  encoded_answers = "eyIxIjoiY29tbWFuZCBsaW5lIiwiMiI6InZlcnNpb24gY29udHJvbCIsIjMi\nOlsiY29va2llcyIsImNvb2tpZSJdLCI0IjoiNDA0IGVycm9yIiwiNSI6Indl\nYiBzZXJ2ZXIiLCI2IjoiaXAgYWRkcmVzcyIsIjciOiJkbnMiLCI4IjoicmVz\ncG9uc2l2ZSBkZXNpZ24iLCI5IjoiaGV4IiwiMTAiOiJodHRwcyIsIjExIjoi\nb3BlbiBzb3VyY2UiLCIxMiI6ImFqYXgiLCIxMyI6ImphdmFzY3JpcHQiLCIx\nNCI6ImRhdGFiYXNlIiwiMTUiOiJodG1sIiwiMTYiOiJjc3MifQ==\n"
   decoded_json = Base64.decode64(encoded_answers)
   answers = JSON.parse(decoded_json)
-
+  
   answers.each do |number, answer_array|
     define_method("answer_#{number}") do 
       answer_array
