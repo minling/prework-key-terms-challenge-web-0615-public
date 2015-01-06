3db2f2bf16762b0a2874e5a2c49f12ae08aab29c
Katie Hoffman <ktahoffman@gmail.com>
2015-01-06 10:50:01 -0500

    add downcase

--- spec/key_terms_spec.rb
+++ spec/key_terms_spec.rb
@@ -1,12 +1,13 @@
 describe "Key Terms Challenge" do
+
   it 'is a text-only way of browsing and interacting with your computer' do
     your_answer = __
-    expect(your_answer).to eq(answer_1)
+    expect(your_answer.downcase).to eq(answer_1)
   end
 
   it 'keeps track of the changes you make to a file' do
     your_answer = __
-    expect(your_answer).to eq(answer_2)
+    expect(your_answer.downcase).to eq(answer_2)
   end
 
   it 'downloads onto your computer to keep track of what you do on its site' do
@@ -16,67 +17,67 @@ describe "Key Terms Challenge" do
 
   it 'is the status code for “website not found”' do
     your_answer = __
-    expect(your_answer).to eq(answer_4)
+    expect(your_answer.downcase).to eq(answer_4)
   end
 
   it 'is a computer that can be accessed through the Internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_5)
+    expect(your_answer.downcase).to eq(answer_5)
   end
 
   it "is your computer's address on the internet" do 
     your_answer = __
-    expect(your_answer).to eq(answer_6)
+    expect(your_answer.downcase).to eq(answer_6)
   end
 
   it 'keeps track of domain names and IP address on the internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_7)
+    expect(your_answer.downcase).to eq(answer_7)
   end
 
   it 'is a way of designing a web page to be easily viewable on multiple screen sizes' do 
     your_answer = __
-    expect(your_answer).to eq(answer_8)
+    expect(your_answer.downcase).to eq(answer_8)
   end
 
   it 'is the way web browsers describe colors' do 
     your_answer = __
-    expect(your_answer).to eq(answer_9)
+    expect(your_answer.downcase).to eq(answer_9)
   end
 
   it 'is the secure way data is moved around the internet' do 
     your_answer = __
-    expect(your_answer).to eq(answer_10)  
+    expect(your_answer.downcase).to eq(answer_10)  
   end
 
   it 'is the practice of sharing how a computer program was made and allowing anyone to customize the program as they see fit' do 
     your_answer = __
-    expect(your_answer).to eq(answer_11)  
+    expect(your_answer.downcase).to eq(answer_11)  
   end
 
   it 'makes browser requests to the server asynchronous' do 
     your_answer = __
-    expect(your_answer).to eq(answer_12)  
+    expect(your_answer.downcase).to eq(answer_12)  
   end
 
   it 'is the programming language of the browser' do 
     your_answer = __
-    expect(your_answer).to eq(answer_13)  
+    expect(your_answer.downcase).to eq(answer_13)  
   end
 
   it 'holds all the data in your application' do 
     your_answer = __
-    expect(your_answer).to eq(answer_14)  
+    expect(your_answer.downcase).to eq(answer_14)  
   end
 
   it "is the language to build a website's content" do 
     your_answer = __
-    expect(your_answer).to eq(answer_15)  
+    expect(your_answer.downcase).to eq(answer_15)  
   end
 
   it "is the language to style a website's content" do 
     your_answer = __
-    expect(your_answer).to eq(answer_16)  
+    expect(your_answer.downcase).to eq(answer_16)
   end
 
 end
